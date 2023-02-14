1e8bcea3d24e0dc9b74cb9a72f6d4255bbda266f
RkDx <rkd@rkdx.local>
2023-02-14 21:43:59 +0100

    Changes 3

--- Rubeus/Domain/Info.cs
+++ Rubeus/Domain/Info.cs
@@ -6,19 +6,31 @@ namespace Myruby.Domain
     {
         public static void ShowLogo()
         {
-            Console.WriteLine("\r\n   ______        _                      ");
-            Console.WriteLine("  (_____ \\      | |                     ");
-            Console.WriteLine("   _____) )_   _| |__  _____ _   _  ___ ");
-            Console.WriteLine("  |  __  /| | | |  _ \\| ___ | | | |/___)");
-            Console.WriteLine("  | |  \\ \\| |_| | |_) ) ____| |_| |___ |");
-            Console.WriteLine("  |_|   |_|____/|____/|_____)____/(___/\r\n");
-            Console.WriteLine("  vX.X.X \r\n");
+            string mylogo = @"                                                                                
+                 ################.                
+             #######################              
+          ###########        *###                 
+        .#######*                      .##        
+       #######                       ######       
+      *######           .##  *###    #######      
+      ######          #####%##        ######      
+      ######       *###  #####        ######,     
+      ######     ###     ##  ###      ######      
+       ######                        #######      
+       ,#####                       #######       
+         ##                      %#######,        
+                 #####*     ###########(          
+              ######################%             
+                  ##############.                                                            
+";
+            Console.WriteLine(mylogo);
+            Console.WriteLine(" MyRuby vX.X.X \r\n");
         }
 
         public static void ShowUsage()
         {
             string usage = @"
-!!!!!!!!!!!!!!!!! READ THE FUCKIN MANUAL !!!!!!!!!!!!!!!!!
+############### READ THE FUCKIN MANUAL ###############
 ";
             Console.WriteLine(usage);
         }
--- Rubeus/Properties/AssemblyInfo.cs
+++ Rubeus/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("658c8b7f-3664-4a95-9572-a3e5871dfc06")]
+[assembly: Guid("948c8b7f-3664-4a95-9572-a3e5871dfb07")]
 
 // Version information for an assembly consists of the following four values:
 //
